9e12d5070489aa804a6a2bdd9c598a54e803b0c9
Alejandro Franco <alejo4373@gmail.com>
2025-03-01 09:00:51 -0500

    Add bg for rutas and adjust index bg

---
+++ assets/body-background-sizer.js
@@ -0,0 +1,50 @@
+let timeout = undefined;
+
+const resizeBg = () => {
+  const firstTwoSectionsHeight = Array.from(
+    document.querySelectorAll("section"),
+  )
+    .slice(0, 2)
+    .map((section) => section.getBoundingClientRect().height)
+    .reduce((a, b) => a + b);
+
+  const headerMenuHeight = document
+    .querySelector(".shopify-section-group-header-group")
+    .getBoundingClientRect().height;
+
+  const bodyHeight = document
+    .querySelector("body")
+    .getBoundingClientRect().height;
+
+  const bgHeight = firstTwoSectionsHeight + headerMenuHeight;
+
+  // Only display hero background if page is the index page
+  if (window.location.pathname === "/") {
+    const heroBgImg = document.querySelector(".hero-bg img");
+    heroBgImg.style.setProperty("height", `${bgHeight}px`);
+
+    document.querySelector(".hero-bg").style.setProperty("display", "block");
+  }
+
+  // Set the height of the bg-texture to the height of the body
+  // so that the texture covers the whole page background
+  document
+    .querySelector(".bg-texture")
+    .style.setProperty("height", `${bodyHeight}px`);
+
+  // Hide bg-texture on rutas pages because they have their own background
+  if (
+    ["/pages/rutas", "/pages/ruta-la-alacena", "/pages/ruta-san-luis"].includes(
+      window.location.pathname,
+    )
+  ) {
+    document.querySelector(".bg-texture").style.setProperty("display", "none");
+  }
+};
+
+window.addEventListener("resize", () => {
+  clearTimeout(timeout);
+  timeout = setTimeout(resizeBg, 250);
+});
+
+resizeBg();
